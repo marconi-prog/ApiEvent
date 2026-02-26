843cd0825bedf25583a0b93b0b5f9f4bb1d6469e
Marconi-prog <marconifarias82@gmail.com>
2026-02-25 21:06:00 -0300

    fix: the yaml and the models

---
+++ src/main/resources/db/migration/V1__create_event_table.sql
@@ -0,0 +1,18 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE event (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+
+    title VARCHAR(100) NOT NULL,
+
+    description VARCHAR(200) NOT NULL,
+
+    img_url VARCHAR(100) NOT NULL,
+
+    event_url VARCHAR(100) NOT NULL,
+
+    date TIMESTAMP NOT NULL,
+
+    remote BOOLEAN NOT NULL
+
+)
